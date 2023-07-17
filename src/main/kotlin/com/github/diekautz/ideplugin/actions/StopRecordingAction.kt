6cf66c39d5b47a3b953884404160aab822716492
Malte Stellmacher <Malte.Stellmacher@student.hpi.de>
2023-07-17 15:42:46 +0200

    Add actions for starting and stopping a recording

---
+++ src/main/kotlin/com/github/diekautz/ideplugin/actions/StopRecordingAction.kt
@@ -0,0 +1,17 @@
+package com.github.diekautz.ideplugin.actions
+
+import com.github.diekautz.ideplugin.services.MyTobiiProService
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.components.service
+
+class StopRecordingAction : AnAction() {
+    override fun update(e: AnActionEvent) {
+        val currentProject = e.project
+        e.presentation.isEnabledAndVisible = currentProject != null
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+        e.project?.service<MyTobiiProService>()?.stopRecording()
+    }
+}

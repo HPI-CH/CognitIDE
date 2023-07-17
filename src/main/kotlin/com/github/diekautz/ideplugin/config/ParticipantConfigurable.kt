a1b105f136e36d79ff7bb8efeac8fd43a8f341f7
Malte Stellmacher <Malte.Stellmacher@student.hpi.de>
2023-07-17 20:19:10 +0200

    Add participant configuration

---
+++ src/main/kotlin/com/github/diekautz/ideplugin/config/ParticipantConfigurable.kt
@@ -0,0 +1,131 @@
+package com.github.diekautz.ideplugin.config
+
+import com.intellij.openapi.options.BoundConfigurable
+import com.intellij.ui.dsl.builder.*
+import java.time.Year
+
+class ParticipantConfigurable : BoundConfigurable(
+    "ParticipantConfigurable"
+) {
+    private val model = ParticipantState.instance
+
+    override fun createPanel() = panel {
+        collapsibleGroup("Participant") {
+            row("Id:") {
+                intTextField().bindIntText(model::id)
+            }
+        }
+        group("Traits") {
+            row("Gender:") {
+                comboBox(listOf("male", "female", "non-binary", "prefer not to disclose"))
+                    .bindItem(model::gender.toNullableProperty())
+            }
+            row("Profession:") {
+                textField().bindText(model::profession)
+            }
+            row("Handedness:") {
+                comboBox(listOf("left", "right", "mixed", "ambidextrous"))
+                    .bindItem(model::handedness.toNullableProperty())
+            }
+        }
+        group("How do you estimate") {
+            row {
+                comboBox((1..10).toList())
+                    .label("your programming experience?")
+                    .bindItemNullable(model::experience10)
+            }.layout(RowLayout.PARENT_GRID)
+            row {
+                comboBox((1..5).toList())
+                    .label("your programming experience compared to \nexperts with 20 years of practical experience?")
+                    .bindItemNullable(model::compareExpert5)
+            }.layout(RowLayout.PARENT_GRID)
+            row {
+                comboBox((1..5).toList())
+                    .label("your programming experience compared to \nyour class mates?")
+                    .bindItemNullable(model::compareClassmates5)
+            }.layout(RowLayout.PARENT_GRID)
+        }
+
+        group("How experienced are you with the following languages:") {
+            row {
+                comboBox((1..5).toList())
+                    .label("Java")
+                    .bindItemNullable(model::experienceJava5)
+            }.layout(RowLayout.PARENT_GRID)
+            row {
+
+                comboBox((1..5).toList())
+                    .label("C")
+                    .bindItemNullable(model::experienceC5)
+            }.layout(RowLayout.PARENT_GRID)
+            row {
+                comboBox((1..5).toList())
+                    .label("Haskell")
+                    .bindItemNullable(model::experienceHaskell5)
+            }.layout(RowLayout.PARENT_GRID)
+            row {
+                comboBox((1..5).toList())
+                    .label("Prolog")
+                    .bindItemNullable(model::experienceProlog5)
+            }.layout(RowLayout.PARENT_GRID)
+            row {
+                textField()
+                    .label("Other laguages")
+                    .bindText(model::additionalLanguages)
+            }.layout(RowLayout.PARENT_GRID)
+        }
+        group("How experienced are you with the following programming paradigms:") {
+            row {
+                comboBox((1..5).toList())
+                    .label("Functional programming")
+                    .bindItemNullable(model::paradigmFunctional5)
+            }.layout(RowLayout.PARENT_GRID)
+            row {
+                comboBox((1..5).toList())
+                    .label("Imperative programming")
+                    .bindItemNullable(model::paradigmImperative5)
+            }.layout(RowLayout.PARENT_GRID)
+            row {
+                comboBox((1..5).toList())
+                    .label("Logical programming")
+                    .bindItemNullable(model::paradigmLogical5)
+            }.layout(RowLayout.PARENT_GRID)
+            row {
+                comboBox((1..5).toList())
+                    .label("Object-oriented programming")
+                    .bindItemNullable(model::paradigmOOP5)
+            }.layout(RowLayout.PARENT_GRID)
+        }
+        group("For how many years have you been...") {
+            row {
+                intTextField(0..100)
+                    .label("programming in general?")
+                    .bindIntText(model::yearsProgramming)
+            }.layout(RowLayout.PARENT_GRID)
+            row {
+                intTextField(0..100)
+                    .label("for larger software projects, e.g., in a company?")
+                    .bindIntText(model::yearsProgrammingCompany)
+            }.layout(RowLayout.PARENT_GRID)
+        }
+        group("University") {
+            row {
+                intTextField(1950.rangeTo(Year.now().value))
+                    .label("Enrollment year")
+                    .bindIntText(model::enrollYear)
+            }
+            row {
+                intTextField(0..100)
+                    .label("How many courses did you take in which you had to implement source code?")
+                    .bindIntText(model::coursesCoding)
+            }
+        }
+        group("How old are you?") {
+            row {
+                intTextField(1..100)
+                    .bindIntText(model::age)
+            }
+        }
+
+    }
+}
---
+++ src/main/kotlin/com/github/diekautz/ideplugin/config/ParticipantState.kt
@@ -0,0 +1,53 @@
+package com.github.diekautz.ideplugin.config
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.util.xmlb.XmlSerializerUtil
+import kotlin.random.Random
+
+@State(
+    name = "com.github.diekautz.ideplugin.config.ParticipantState",
+    storages = [Storage("OpenEyePlugin_participant.xml")]
+)
+class ParticipantState : PersistentStateComponent<ParticipantState> {
+    var id: Int = Random.nextInt().mod(10000)
+
+    // traits
+    var gender: String? = null
+    var profession: String = ""
+    var handedness: String? = null
+
+    // programming questionaire
+    var experience10: Int? = null
+    var compareExpert5: Int? = null
+    var compareClassmates5: Int? = null
+
+    var experienceJava5: Int? = null
+    var experienceC5: Int? = null
+    var experienceHaskell5: Int? = null
+    var experienceProlog5: Int? = null
+    var additionalLanguages = ""
+
+    var paradigmFunctional5: Int? = null
+    var paradigmLogical5: Int? = null
+    var paradigmImperative5: Int? = null
+    var paradigmOOP5: Int? = null
+    var yearsProgramming = 0
+    var yearsProgrammingCompany = 0
+    var enrollYear = 0
+    var coursesCoding = 0
+    var age: Int = 0
+
+    override fun getState(): ParticipantState = this
+
+    override fun loadState(state: ParticipantState) {
+        XmlSerializerUtil.copyBean(state, this)
+    }
+
+    companion object {
+        val instance: ParticipantState
+            get() = ApplicationManager.getApplication().getService(ParticipantState::class.java)
+    }
+}

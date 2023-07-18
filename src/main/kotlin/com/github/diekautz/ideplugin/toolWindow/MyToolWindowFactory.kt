c434125a79df0ef0f75a0aca7d7021fe658fa310
Malte Stellmacher <malte.stellmacher@student.hpi.de>
2023-07-19 00:07:28 +0200

    Revert "Remove deprecated Cell.horizontalAlign()"
    This reverts commit 30996669f31beba645b9e6ff09d6ea0f32c19cc2.

--- src/main/kotlin/com/github/diekautz/ideplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/diekautz/ideplugin/toolWindow/MyToolWindowFactory.kt
@@ -12,10 +12,10 @@ import com.intellij.openapi.ui.MessageDialogBuilder
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
-import com.intellij.ui.dsl.builder.Align
 import com.intellij.ui.dsl.builder.bindIntText
 import com.intellij.ui.dsl.builder.bindText
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.ui.dsl.gridLayout.HorizontalAlign
 import com.intellij.ui.table.JBTable
 
 
@@ -104,7 +104,8 @@ class MyToolWindowFactory : ToolWindowFactory {
             group("2: Select Device") {
 
                 row {
-                    scrollCell(table).align(Align.FILL)
+                    scrollCell(table)
+                        .horizontalAlign(HorizontalAlign.FILL)
                 }
                 row {
                     text("Test")

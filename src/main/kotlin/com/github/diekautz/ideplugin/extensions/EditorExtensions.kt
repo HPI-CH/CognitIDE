6bd260b28c6ffee66c29b5e6b74f53c17eef5478
Malte Stellmacher <Malte.Stellmacher@student.hpi.de>
2023-07-24 17:03:56 +0200

    Remove unused extension functions

--- src/main/kotlin/com/github/diekautz/ideplugin/extensions/EditorExtensions.kt
+++ src/main/kotlin/com/github/diekautz/ideplugin/extensions/EditorExtensions.kt
@@ -1,14 +1,9 @@
 package com.github.diekautz.ideplugin.extensions
 
-import com.github.diekautz.ideplugin.ui.OpenEyeColors
-import com.intellij.codeInsight.highlighting.HighlightManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.LogicalPosition
 import com.intellij.openapi.fileEditor.FileEditor
-import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiDocumentManager
-import com.intellij.psi.PsiElement
 import com.intellij.util.ui.ImageUtil
 import java.awt.Point
 import java.awt.image.BufferedImage
@@ -18,33 +13,6 @@ fun Editor.xyScreenToLogical(point: Point): LogicalPosition {
     val relativePoint = Point(point)
     SwingUtilities.convertPointFromScreen(relativePoint, contentComponent)
     return xyToLogicalPosition(relativePoint)
-}
-
-fun Editor.highlightElements(index: Int, elements: List<PsiElement>, project: Project) {
-    val editorPsiFile = PsiDocumentManager.getInstance(project).getPsiFile(document)
-    val filteredElement = elements.filter {
-        it.containingFile == editorPsiFile
-    }.toTypedArray()
-
-    HighlightManager.getInstance(project).addOccurrenceHighlights(
-        this,
-        filteredElement,
-        OpenEyeColors.LOOKED_ATTRIBUTES[index],
-        true,
-        null
-    )
-}
-
-fun Editor.highlightElementGazePoints(seen: Map<PsiElement, Double>, project: Project) {
-    val assignedColors = OpenEyeColors.assignColors(seen)
-
-    assignedColors.forEach { (colorIndex, entries) ->
-        highlightElements(
-            colorIndex,
-            entries,
-            project
-        )
-    }
 }
 
 fun FileEditor.screenshot(): BufferedImage {

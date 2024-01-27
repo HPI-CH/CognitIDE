46cf73f97e7517cde5c7f29842912a2c70b0b571
unknown <fabian.stolp@student.hpi.uni-potsdam.de>
2024-01-27 21:38:57 +0100

    restructure settings page

--- src/main/kotlin/com/github/hpich/cognitide/config/CognitIDESettingsConfigurable.kt
+++ src/main/kotlin/com/github/hpich/cognitide/config/CognitIDESettingsConfigurable.kt
@@ -40,7 +40,42 @@ class CognitIDESettingsConfigurable : BoundConfigurable(
                 }
             }.enabledIf(interruptCheckBox.selected)
         }
-        group("Device Streams"){
+        group("TobiiPro Eye Tracker") {
+            row("Use devices:") {
+                checkBox("Yes").bindSelected(model::includeTobii)
+                    .comment("Should data from devices of this type be recorded?")
+            }
+            row("Connector path:") {
+                textFieldWithBrowseButton(fileChooserDescriptor = FileChooserDescriptorFactory.createSingleFileOrExecutableAppDescriptor())
+                    .bindText(model::tobiiProConnectorExecutable)
+                    .comment(
+                        "Please <a href='https://labstreaminglayer.readthedocs.io/info/supported_devices.html'>build the TobiiPro Connector</a>. " +
+                                "It is used to create an LSL stream for the eye tracker. " +
+                                "It will be used by the plugin to get the required data.\n" +
+                                "Provide the application path <i>optionally</i> so the application can be opened when needed."
+                    )
+            }
+            group("Eye Tracker Manager") {
+                row("Path:") {
+                    textFieldWithBrowseButton(fileChooserDescriptor = FileChooserDescriptorFactory.createSingleFileOrExecutableAppDescriptor())
+                        .bindText(model::eyeTrackerManagerExecutable)
+                        .comment(
+                            "<i>optional:</i> Provide the path to your " +
+                                    "<a href='https://www.tobii.com/products/software/applications-and-developer-kits/tobii-pro-eye-tracker-manager'>Tobii Pro Eye Tracker Manager</a>. " +
+                                    "It will be called on the fly to setup the calibration of a new participant."
+                        )
+                }
+                row("Serial number:") {
+                    textField()
+                        .bindText(model::eyeTrackerSerial)
+                        .comment(
+                            "<i>optional:</i> Provide the serial number of your tobii pro device (e.g. TPNA1-030109123456.\n" +
+                                    "If provided, the calibration will be opened directly."
+                        )
+                }
+            }
+        }
+        group("Other Device Streams") {
             // Add a panel for each device spec in the model
             model.devices.forEachIndexed { index, deviceSpec ->
                 deviceSpecPanel(this, deviceSpec, index)
@@ -50,43 +85,6 @@ class CognitIDESettingsConfigurable : BoundConfigurable(
                 button("Add Stream") {
                     model.devices.add(DeviceSpec("", "", ""))
                     // Refresh the settings panel to show the new spec
-                }
-            }
-        }
-        group("External Applications") {
-            group("TobiiPro") {
-                row("Use devices:") {
-                    checkBox("Yes").bindSelected(model::includeTobii)
-                        .comment("Should data from devices of this type be recorded?")
-                }
-                row("Connector path:") {
-                    textFieldWithBrowseButton(fileChooserDescriptor = FileChooserDescriptorFactory.createSingleFileOrExecutableAppDescriptor())
-                        .bindText(model::tobiiProConnectorExecutable)
-                        .comment(
-                            "Please <a href='https://labstreaminglayer.readthedocs.io/info/supported_devices.html'>build the TobiiPro Connector</a>. " +
-                                    "It is used to create an LSL stream for the eye tracker. " +
-                                    "It will be used by the plugin to get the required data.\n" +
-                                    "Provide the application path <i>optionally</i> so the application can be opened when needed."
-                        )
-                }
-                group("Eye Tracker Manager") {
-                    row("Path:") {
-                        textFieldWithBrowseButton(fileChooserDescriptor = FileChooserDescriptorFactory.createSingleFileOrExecutableAppDescriptor())
-                            .bindText(model::eyeTrackerManagerExecutable)
-                            .comment(
-                                "<i>optional:</i> Provide the path to your " +
-                                        "<a href='https://www.tobii.com/products/software/applications-and-developer-kits/tobii-pro-eye-tracker-manager'>Tobii Pro Eye Tracker Manager</a>. " +
-                                        "It will be called on the fly to setup the calibration of a new participant."
-                            )
-                    }
-                    row("Serial number:") {
-                        textField()
-                            .bindText(model::eyeTrackerSerial)
-                            .comment(
-                                "<i>optional:</i> Provide the serial number of your tobii pro device (e.g. TPNA1-030109123456.\n" +
-                                        "If provided, the calibration will be opened directly."
-                            )
-                    }
                 }
             }
         }

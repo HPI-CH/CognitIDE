362f947d7b97b91291dd61996a4e977c13ff0878
Lara Kursawe <lara.kursawe@student.hpi.uni-potsdam.de>
2024-07-18 15:23:52 +0200

    Cleanup Study Workflow and Questionnaires

--- src/main/kotlin/com/github/hpich/cognitide/config/questionnaires/PostQuestionnaireConfigurable.kt
+++
@@ -1,9 +0,0 @@
-package com.github.hpich.cognitide.config.questionnaires
-
-import com.github.hpich.cognitide.config.study.StudyState
-import com.github.hpich.cognitide.utils.readJson
-
-class PostQuestionnaireConfigurable : QuestionnaireConfigurable() {
-    override val questionnaire =
-        readJson(StudyState.instance.postStudyQuestionnaireJsonPath)
-}
